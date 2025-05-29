b79caa2c0076492515f4e1883537314c97edf240
Gerardo Arzate <116304288+gerardoarzate@users.noreply.github.com>
2025-05-29 16:17:59 -0600

    Conectar página de rastreo a la API

--- js/rastreo/script.js
+++ js/rastreo/script.js
@@ -1,37 +1,111 @@
 const steps = document.querySelectorAll('.item');
 const statusText = document.querySelector('.status');
-const statusCard = document.querySelector('.status-card');
-const statusDate = statusCard.querySelectorAll('p')[1];
+const statusDateText = document.querySelector('#fechaStatus');
+const statusTimeText = document.querySelector('#horaStatus');
+const trackingIdText = document.querySelector('#folio');
+const stepTimeTexts = document.querySelectorAll('.time');
+const url = new URL(location.href);
+const trackingId = url.searchParams.get('folio');
 
-const estados = [
-  { texto: "Pendiente", clase: "pendiente" },
-  { texto: "Enviado", clase: "enviado" },
-  { texto: "Enviado", clase: "enviado" },
-  { texto: "Entregado", clase: "entregado" }
-];
+function showData({ date, time, status }) {
+  const { shortStatus, stepIndex } = getStatusData(status);
+  const timeFormatted = time.replace(/:[^:]*$/, '');
+  trackingIdText.textContent = trackingId;
+  statusDateText.textContent = date;
+  statusTimeText.textContent = timeFormatted;
+  statusText.classList = `status ${shortStatus.toLowerCase()}`;
+  statusText.textContent = shortStatus;
+
+  stepTimeTexts.forEach(el => {
+    const dateSplit = date.split(' ');
+    el.innerHTML = `${dateSplit[1]} ${dateSplit[2]}<br>${timeFormatted}`
+  });
+
+  activateStep(stepIndex);
+}
+
+function activateStep(stepIndex) {
+  const currentStep = steps[stepIndex];
+
+  // Resetear estilos
+  steps.forEach(s => {
+    s.classList.remove('current', 'completed');
+    s.querySelector('.circle').classList.remove('active');
+  });
+
+  // Activar paso actual y los anteriores
+  for (let i = 0; i <= stepIndex; i++) {
+    steps[i].classList.add('completed');
+  }
+
+  currentStep.classList.add('current');
+  currentStep.querySelector('.circle').classList.add('active');
+}
+
+function getStatusData(longStatus) {
+  const formatted = longStatus.toLowerCase().trim().replace(/\.$/, '');
+  
+  const data = {
+    'pedido registrado': {
+      shortStatus: 'Pendiente',
+      stepIndex: 0
+    },
+    'paquete ha sido enviado': {
+      shortStatus: 'Enviado',
+      stepIndex: 1
+    },
+    'aduana completada': {
+      shortStatus: 'Enviado',
+      stepIndex: 2
+    },
+    'paquete entregado': {
+      shortStatus: 'Entregado',
+      stepIndex: 3
+    }
+  }
+
+  return data[formatted];
+}
+
+async function changeStep(newStepIndex) {
+  const statuses = [
+    'Pedido registrado.',
+    'Paquete ha sido enviado.',
+    'Aduana completada.',
+    'Paquete entregado.'
+  ];
+
+  const body = {
+    estado: statuses[newStepIndex]
+  }
+
+  try {
+    await fetchApi(`tracking/${trackingId}/status`, 'PUT', body);
+    fetchCurrentStep();
+  } catch (error) {
+    alert('Ocurrió un error al cambiar el estado del producto.');
+    console.error(error);
+  }
+}
+
+async function fetchCurrentStep() {
+  try {
+    const res = await fetchApi(`/tracking/${trackingId}`);
+    const data = res.body;
+    showData({
+      date: data.fecha,
+      status: data.estatus,
+      time: data.hora
+    });
+  } catch(error) {
+    alert('No ha sido posible obtener los datos de rastreo');
+    console.error(error);
+  }
+
+}
 
 steps.forEach((step, index) => {
-  step.querySelector('.circle').addEventListener('click', () => {
-    // Resetear estilos
-    steps.forEach(s => {
-      s.classList.remove('current');
-      s.querySelector('.circle').classList.remove('active');
-    });
-
-    // Activar paso actual y los anteriores
-    for (let i = 0; i <= index; i++) {
-      steps[i].classList.add('completed');
-    }
-
-    step.classList.add('current');
-    step.querySelector('.circle').classList.add('active');
-
-    // Cambiar estado
-    statusText.textContent = estados[index].texto;
-    statusText.className = `status ${estados[index].clase}`;
-
-    // Fecha de la burbuja
-    const time = step.querySelector('.time').innerHTML.replace('<br>', ' ');
-    statusDate.textContent = time;
-  });
+  step.querySelector('.circle').addEventListener('click', () => changeStep(index));
 });
+
+fetchCurrentStep();
